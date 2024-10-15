393748808360d11ee91dbce6606701fac7dfb3b9
sekgothetshepang <sekgothetshepang30@gmail.com>
2024-10-15 13:47:08 +0200

    Update server.js

--- server.js
+++ server.js
@@ -18,9 +18,11 @@ app.use(cors());
 app.use(bodyParser.json());
 
 // Connect to MongoDB
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => console.log('MongoDB connected'))
-    .catch(err => console.error('MongoDB connection error:', err));
+
+mongoose.connect('your-mongodb-connection-string')
+  .then(() => console.log('MongoDB connected successfully'))
+  .catch(err => console.error('MongoDB connection error:', err));
+
 
 // Routes
 app.use('/api/comments', commentsRouter);
