aed9052c397941b412bb42ba7889cec51c155b2d
sekgothetshepang <sekgothetshepang30@gmail.com>
2024-10-15 19:51:01 +0200

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,6 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const dotenv = require('dotenv');
 const commentsRoutes = require('./routes/comments');
-
 const commentsRouter = require('./routes/comments');
 
 dotenv.config();
@@ -14,15 +13,17 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 8080;
 
-
 // Middleware
 app.use(cors());
 app.use(bodyParser.json());
 
-// Connect to MongoDB
-
-mongoose.connect('your-mongodb-connection-string')
-  .then(() => console.log('MongoDB connected successfully'))
+// MongoDB connection setup
+// Replace 'myDatabase' with the name of your MongoDB database
+mongoose.connect('mongodb://localhost:27017/techwise-co-', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+  .then(() => console.log('Connected to MongoDB'))
   .catch(err => console.error('MongoDB connection error:', err));
 
 // Basic route
@@ -30,14 +31,11 @@ app.get('/', (req, res) => {
   res.send('Welcome to the Tech Wise API!');
 });
 
-
 // Routes
 app.use('/api/comments', commentsRouter);
 app.use('/comments', commentsRoutes);
 
-
-
 // Start the server
 app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
