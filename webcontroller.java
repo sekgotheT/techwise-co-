fb8d81c2a8b7b6e78cd611e28032e162c0ce6876
sekgothetshepang <sekgothetshepang30@gmail.com>
2024-10-15 22:30:05 +0200

    Update webcontroller.java

--- webcontroller.java
+++ webcontroller.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 @CrossOrigin(origins = "*") // Allow requests from any origin
 @RestController
 public class WebController {
+
     @GetMapping("/api/message")
     public String getMessage() {
         return "Hello from the back-end!";
